ed101eba06cb71aeb13b963dc3ec8c3337385b4a
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 00:22:22 +0100

    1: Execute Insert

---
+++ 1-mysqldb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Execute Insert / Single Substitution Query
+"""
+
+from db_connect import db
+
+cur = db.cursor()
+users = ['John', 'Maxwell', 'Joel', 'David', 'Abraham']
+for user in users:
+	cur.execute("INSERT INTO users (name) VALUES (%s)", (user,))
+	print("New User: {}, with ID: {}".format(user, cur.lastrowid))
