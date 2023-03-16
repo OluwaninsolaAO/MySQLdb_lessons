5d39a465e3c63e36ab6758402e0f2656ecce6c86
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 00:13:13 +0100

    0: Simple Create Table Query

---
+++ 0-mysqldb.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Create a simple table using the db object.
+Table Name: users
+Fields: id and name
+"""
+
+from db_connect import db  # imports db from db_connect
+
+cur = db.cursor()  # get a default cursor class
+cur.execute("CREATE TABLE IF NOT EXISTS users ( id INT PRIMARY KEY AUTO_INCREMENT, name CHAR(255) NOT NULL )")  # executes mysql query
