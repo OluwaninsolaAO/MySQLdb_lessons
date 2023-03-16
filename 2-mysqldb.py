4ef4dcc7d76560588fad3b215c5cbb655af9856c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 00:46:20 +0100

    2: Update 2-mysqldb to display query result

---
+++ 2-mysqldb.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Multiple Substitution Query.
+"""
+
+from db_connect import db
+
+cur = db.cursor()
+cur.execute("SELECT * FROM users WHERE id BETWEEN %s AND %s", ( 5 , 15 ))
+rows = cur.fetchall()
+for row in rows:
+	print(row)
