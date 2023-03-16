4e5cb3e7d6102f5cce9650df9cbbfdb0d3676a84
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 00:06:16 +0100

    Update db_connect doc string

--- db_connect.py
+++ db_connect.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""
+This file connects to MySQLdb and creates a `db` object,
+written once and can be used and reused anywhere in the
+project.
+"""
+
 import MySQLdb
 
 db = MySQLdb.connect(host='localhost', user='superuser', password='superuser', db='test_db')
-cur = db.cursor()
