e7ff1c3fde457886d9456401b4f2e3dbac6af650
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 00:58:47 +0100

    4: Fetch One-at-a-Time

---
+++ 4-mysqldb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Fetch One-at-a-Time"""
+
+from db_connect import db
+
+cur = db.cursor()
+cur.execute("SELECT * FROM users WHERE id = %s", (15,))
+result = cur.fetchone()
+print("id: {} -- name: {}".format(result[0], result[1]))
