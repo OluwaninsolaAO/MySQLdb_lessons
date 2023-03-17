d6a3b4c0f308112b259baa0b8398d1119f699977
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 08:18:48 +0100

    lesson 1: create a script that imports and create a class object from mysqlalchemy orm

---
+++ sqlalchemy_orm/1-sqlalchemy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = sqlalchemy.orm.declarative_base()
+
+print(type(engine))
+print(type(Base))
