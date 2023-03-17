86b58d4bdbab609182d02cd54f518e8199b3ce6c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 13:09:10 +0100

    Update base_model to include execute create tables in database

--- sqlalchemy_orm/base_model.py
+++ sqlalchemy_orm/base_model.py
@@ -20,3 +20,6 @@ class User(Base):
 
     def __repr__(self):
         return "[User: <name: {}> <age: {}>]".format(self.name, self.age)
+
+# Create tables in database
+Base.metadata.create_all(engine)
